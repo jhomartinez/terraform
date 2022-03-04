4abb874e943f444fd5e032800d481210253a1ae2
Jhon <jhon@mnatg.com>
2022-03-04 03:23:31 +0000

    add the vpc default and security grops with ingress from http and https  with terraform

--- main.tf
+++ main.tf
@@ -8,3 +8,27 @@ resource "aws_instance" "prod"{
     instance_type = "t2.micro"
     ami = "ami-08b6f2a5c291246a0"
 }
+
+data "aws_vpc" "default" {
+    id = "vpc-8cae28e7"
+}
+
+resource "aws_security_group" "http_https" {
+    name = "http_https"
+    description = "Allows http and https trafics only"
+    vpc_id = data.aws_vpc.default.id
+    ingress {
+        description = "Https from the internet"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = [data.aws_vpc.default.cidr_block, "0.0.0.0/0"]
+    }
+    ingress {
+        description = "Http from the internet"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = [data.aws_vpc.default.cidr_block, "0.0.0.0/0"]
+    }
+}
