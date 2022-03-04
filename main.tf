76586d701316eae3185869cf6cb21937648eed5f
Jhon <jhon@mnatg.com>
2022-03-04 03:08:32 +0000

    create the first ec2 with terraform

--- main.tf
+++ main.tf
@@ -3,3 +3,8 @@ provider "aws"{
     profile = "default"
     shared_credentials_file = "~/.aws/credentials"
 }
+
+resource "aws_instance" "prod"{
+    instance_type = "t2.micro"
+    ami = "ami-08b6f2a5c291246a0"
+}
